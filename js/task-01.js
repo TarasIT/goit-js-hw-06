b0cfb7c735fecf5cde348a7f2fd16eced5b85663
Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
2022-07-29 17:14:49 +0300

    first task done

---
+++ js/task-01.js
@@ -0,0 +1,35 @@
+// ======================= ЗАДАНИЕ 1 ===========================
+
+const categoriesCount = document.querySelectorAll("li.item").length;
+console.log("Number of categories:", categoriesCount);
+
+// -----------------------Animals
+
+const firstCategoryList =
+  document.body.firstElementChild.nextElementSibling.firstElementChild;
+
+const firstCategoryTitle = firstCategoryList.querySelector("h2").textContent;
+console.log("Category:", firstCategoryTitle);
+
+const animalsCount = firstCategoryList.querySelectorAll("li").length;
+console.log("Elements:", animalsCount);
+
+// -----------------------Products
+
+const secondCategoryList = firstCategoryList.nextElementSibling;
+
+const secondCategoryTitle = secondCategoryList.querySelector("h2").textContent;
+console.log("Category:", secondCategoryTitle);
+
+const productsCount = secondCategoryList.querySelectorAll("li").length;
+console.log("Elements:", productsCount);
+
+// -----------------------Technologies
+
+const thirdCategoryList = secondCategoryList.nextElementSibling;
+
+const thirdCategoryTitle = thirdCategoryList.querySelector("h2").textContent;
+console.log("Category:", thirdCategoryTitle);
+
+const technologiesCount = thirdCategoryList.querySelectorAll("li").length;
+console.log("Elements:", technologiesCount);
