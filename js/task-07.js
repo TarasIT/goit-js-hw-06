541124e4dd6cfb5b13552e918e301f42ebb48777
Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
2022-08-02 16:17:15 +0300

    update 7th task

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@ const fontSizeController = document.querySelector("input#font-size-control");
 const textChange = document.querySelector("span#text");
 
 const fontSizeChange = () => {
-  const currentFontSize = fontSizeController.value.toString() + "px";
+  const currentFontSize = fontSizeController.value + "px";
   console.log((textChange.style.fontSize = currentFontSize));
 };
 
