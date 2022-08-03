f2c9389b38a0789a939f69cd0ad8688a950dd72d
Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
2022-08-03 12:34:12 +0300

    9th task is done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const colorValue = document.querySelector("span.color");
+const colorSwitch = document.querySelector("button.change-color");
+const body = document.body;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function colorManager() {
+  colorValue.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+colorSwitch.addEventListener("click", colorManager);
