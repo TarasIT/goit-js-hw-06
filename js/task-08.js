588da838bc9f32d29625febfa3a48dabfca3a874
Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
2022-08-03 10:41:55 +0300

    8th task is done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector("form.login-form");
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+};
+
+loginForm.addEventListener("submit", handleSubmit);
